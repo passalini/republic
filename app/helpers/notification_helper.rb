0002712e0f09f3768d5f3b734eac20783eb5a7f3
Pedro Passalini <henrique.passalini@gmail.com>
2014-09-19 15:57:07 -0300

    added flash notifications snippet

---
+++ app/helpers/notification_helper.rb
@@ -0,0 +1,29 @@
+module NotificationHelper
+  # Inspired by https://github.com/seyhunak/twitter-bootstrap-rails/blob/master/app/helpers/bootstrap_flash_helper.rb
+  def flash_notifications(options = {})
+    flash_messages = []
+
+    flash.each do |type, message|
+      # Skip empty messages, e.g. for devise messages set to nothing in a locale file.
+      next if message.blank?
+
+      # Converting default types
+      type = type.to_sym
+      type = :success if type == :notice
+      type = :danger  if type == :alert
+      type = :danger  if type == :error
+
+      fa = type == :success ? :check : :ban
+
+      Array(message).each do |msg|
+        text = content_tag(:div,
+          content_tag(:i, nil, class: "fa fa-#{fa}") +
+          content_tag(:button, raw("&times;"), :class => "close", "data-dismiss" => "alert") +
+          msg, :class => "alert fade in alert-#{type} #{options[:class]}")
+        flash_messages << text if msg
+      end
+    end
+
+    flash_messages.join("\n").html_safe
+  end
+end
