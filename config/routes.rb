9c1e4b0f26d85ef789771ef40f5d3b2de295e0ff
Pedro Passalini <henrique.passalini@gmail.com>
2014-11-06 22:37:35 -0200

    removed code to scti start

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :group_bills
   get 'fraternities/index'
 
   root to: 'fraternities#index'
--- spec/factories/group_bills.rb
+++
@@ -1,8 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :group_bill do
-    month "MyString"
-    observations "MyText"
-  end
-end
